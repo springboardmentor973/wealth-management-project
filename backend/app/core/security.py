8134a72b46a830b7280283a972cafabc01ae4467
Hariprasad <hariragul2006@gmail.com>
2025-12-17 17:31:07 +0530

    Added function to decode the token

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,5 @@
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, ExpiredSignatureError, JWTError
 from datetime import datetime, timedelta
 from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
@@ -18,3 +18,12 @@ def create_token(data : dict):
     expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES or 30)
     to_encode.update({"expire" : expire})
     return jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+
+def decode_access_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        return payload
+    except ExpiredSignatureError:
+        return {"error" : "Token has expired"}
+    except JWTError:
+        return {"error" : "Invalid token"}
