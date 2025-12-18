930fa254fe64f8dcb21400281216ac783795deca
Hariprasad <hariragul2006@gmail.com>
2025-12-18 17:51:39 +0530

    Fixed the suggestions

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,6 @@
 from passlib.context import CryptContext
-from jose import jwt, ExpiredSignatureError, JWTError
+from jose import jwt
+from jose.exceptions import ExpiredSignatureError, JWTError
 from datetime import datetime, timedelta
 from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
@@ -16,12 +17,12 @@ def verify_password(plain : str, hashed : str) -> bool:
 def create_token(data : dict):
     to_encode = data.copy()
     expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES or 30)
-    to_encode.update({"expire" : expire})
+    to_encode.update({"exp" : expire})
     return jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
 
 def decode_access_token(token):
     try:
-        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, [ALGORITHM])
         return payload
     except ExpiredSignatureError:
         return {"error" : "Token has expired"}
