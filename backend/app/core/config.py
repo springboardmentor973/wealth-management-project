f905ba6b5af22294b729fc3b84cc35086d19dd43
mehtaakirann <kiran.mehta@cumminscollege.edu.i>
2025-12-18 18:16:15 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,6 +12,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(
     os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 )
 
-# Using same secret for now
-REFRESH_SECRET_KEY = SECRET_KEY
+# # Using same secret for now
+# REFRESH_SECRET_KEY = SECRET_KEY
 
