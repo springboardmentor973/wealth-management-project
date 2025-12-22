c573c228ec0012b5afa70a63e28d64dc179e6947
Abhinaya54 <abhinayapulagam@gmail.com>
2025-12-22 17:59:20 +0530

    Day-4: add current_amount field to Goal model

---
+++ backend/app/models/goal.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Float, Date, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from app.database import Base
+
+
+class Goal(Base):
+    __tablename__ = "goals"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    # Link goal to a user
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    goal_type = Column(String, nullable=False)
+    target_amount = Column(Float, nullable=False)
+
+    #  NEW FIELD (Progress Tracking)
+    current_amount = Column(Float, default=0)
+
+    target_date = Column(Date, nullable=False)
+    monthly_contribution = Column(Float, nullable=False)
+
+    # Defaults
+    status = Column(String, default="active")
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
