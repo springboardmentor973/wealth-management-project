fa0498ad728cb97244ad0ceb5e70fa8ea749bf70
Hariprasad <hariragul2006@gmail.com>
2026-01-06 16:47:42 +0530

    Resolved import errors

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,18 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:hanifuddin@localhost:5432/wealth_database"
-DATABASE_URL = "postgresql://postgres:Ak%4026042004@localhost:5432/wealth_database"
-
+###
+#  Swap with your database url
+# DATABASE_URL = "postgresql://postgres:hanifuddin@localhost:5432/wealth_database"
+# DATABASE_URL = "postgresql://postgres:Ak%4026042004@localhost:5432/wealth_database"
+###
+DATABASE_URL = "postgresql://postgres:HariPostgres123@localhost:5432/wealth_database"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-from app.base import Base
+from base import Base
 
-from .models.user import User
-from .models.goal import Goal
-from .models.investment import Investment
-
+from models.user import User
+from models.goal import Goal
+from models.investment import Investment
 
 def init_db():
     try:
