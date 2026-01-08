3ccce05fd2665eb66f472cd403d29d0da196cfcb
mehtaakirann <kiran.mehta@cumminscollege.edu.i>
2026-01-08 18:50:11 +0530

    task 6

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,7 +6,7 @@ from app.models.user import User
 from app.models.goal import Goal
 from app.models.transaction import Transaction
 
-DATABASE_URL = "postgresql://postgres:Ak%4026042004@localhost:5432/wealth_database"
+DATABASE_URL = "postgresql://postgres:kiranm5@localhost:5432/wealth_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
